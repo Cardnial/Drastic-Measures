bfe563b9c15a75854d7d1d04fd81d6eabe3103de
Joshua <45546025+SirJoshi@users.noreply.github.com>
2020-09-06 00:35:20 -0500

    Update Player.gd

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -13,9 +13,12 @@ onready var camera = $Head/Camera
 
 # Witchcraft
 var velocity = Vector3()
+
+# Centers Camera
 var camera_x_rotation = 0
 var camera_y_rotation = 0
 
+# Captures Cursor
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -27,17 +30,21 @@ func _input(event):
 ##		camera.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
 ##		replace camera with head to add a noclip like mode
 		
+		# This Moves the Camera Up Down
 		var x_delta = event.relative.y * mouse_sensitivity
+		# This Makes it So You Have a Spine
 		if camera_x_rotation + x_delta > -90 and camera_x_rotation + x_delta < 90:
 			camera.rotate_x(deg2rad(-x_delta))
 			camera_x_rotation += x_delta
 		
+# Press Esc to Toggle Cursor Lock
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel") and Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif Input.is_action_just_pressed("ui_cancel") and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
+
+# WASD	
 func _physics_process(delta):
 	var head_basis = head.get_global_transform().basis
 	
@@ -51,7 +58,8 @@ func _physics_process(delta):
 		direction -= head_basis.x
 	elif Input.is_action_pressed("move_right"):
 		direction += head_basis.x
-		
+	
+	# This has something to do with movement i swear
 	direction = direction.normalized()
 	
 	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
